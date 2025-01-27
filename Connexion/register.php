e1baf9cd07157d90a5ba9018f05c143070b89000
evomerse <nathandenoeux@outlook.com>
2025-01-27 15:36:02 +0100

    Page de connexion + ajout de php

---
+++ Connexion/register.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    // Connexion à la base de données
+    $conn = new mysqli('localhost', 'root', '', 'servpad');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Compte créé avec succès";
+    } else {
+        echo "Erreur: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
